a522695beb5724c1029eb2123967be64c86ad7ef
spandan1460 <1929250@kiit.ac.in>
2021-09-22 04:27:03 +0530

    Update the page

--- app.js
+++ app.js
@@ -25,12 +25,28 @@ app.get('/', function(req, res) {
 });
 
 
+// About Page Route.
+
+app.get("/about", function(req, res) {
+  res.render('about', {
+    aboutContent: aboutContent
+  });
+});
+
+
+//Contact Page Route.
+
+app.get('/contact', function(req, res){
+  res.render('contact', {
+    contactContent: contactContent
+  });
+});
 
 
 
 
 
-
+//Spinning up the server.
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
