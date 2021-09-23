6df3aaa82acc138eec5ecb0e4a56cf754161ac50
spandan1460 <1929250@kiit.ac.in>
2021-09-23 21:02:57 +0530

    Completed Challenge 8

--- app.js
+++ app.js
@@ -43,8 +43,18 @@ app.get('/contact', function(req, res){
 });
 
 
+//Compose page route.
+
+app.get('/compose', function(req, res){
+  res.render('compose');
+});
 
 
+
+
+app.post('/compose', function(req, res){
+  console.log(req.body.postTitle);
+})
 
 //Spinning up the server.
 
