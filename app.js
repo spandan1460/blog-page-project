d8b872cd4f1987177e4232ff9d5d054d44d64e8c
spandan1460 <1929250@kiit.ac.in>
2021-09-29 11:49:02 +0530

    Completed Challenge 20

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 //jshint esversion:6
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const express = require("express");               //requiring the express node modules
+const bodyParser = require("body-parser");        //requiring the body-parser modules
+const ejs = require("ejs");                       //requiring the ejs modules.
+const _ = require("lodash");                      //requiring the lodash modules.
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -48,14 +49,14 @@ app.get('/contact', function(req, res){
 
 //Posts Route
 app.get('/posts/:postName', function(req, res){
-  const requestedTitle = req.params.postName;
+  const requestedTitle = _.lowerCase(req.params.postName);
  posts.forEach(function(post) {
-   const storedTitle = post.Title;
+   const storedTitle = _.lowerCase(post.Title);
    if(storedTitle === requestedTitle) {
-     console.log("Match Found!");
-   }
-   else{
-     console.log("Match not Found!");
+     res.render('post', {
+       title: post.Title,
+       content: post.content
+     });
    }
  });
 });
